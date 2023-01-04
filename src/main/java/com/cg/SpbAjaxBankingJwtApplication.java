c69f69cd6b243e36567a0bc45d11f60a1c9b279a
nguyenhuytruc <huytruc1201@gmail.com>
2023-01-04 12:52:20 +0700

    banking spb

---
+++ src/main/java/com/cg/SpbAjaxBankingJwtApplication.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbAjaxBankingJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbAjaxBankingJwtApplication.class, args);
+        System.out.println("Success......");
+    }
+
+}
